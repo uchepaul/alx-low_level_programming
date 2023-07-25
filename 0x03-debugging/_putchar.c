0b48329fa1b91c38be9e12701772a87b6bc43ed9
uchepaul <ucheegbuniwe1@gmail.com>
2023-07-25 04:18:24 -0700

    Alx debugging C project

--- 0x03-debugging/_putchar.c
+++
@@ -1,9 +0,0 @@
-// _putchar.c
-
-#include <unistd.h>
-
-// The actual implementation of _putchar that writes a character to the console.
-// In this case, we are using the write system call to output a single character.
-int _putchar(char c) {
-    return write(1, &c, 1);
-}
